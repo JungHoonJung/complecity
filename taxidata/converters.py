9ead6df025b0ea97eef718b46ddc68e142eb01e4
Gnuaz <jh.jung1380@gmail.com>
2019-11-21 21:11:54 +0900

    convert

--- taxidata/converters.py
+++ taxidata/converters.py
@@ -2,8 +2,8 @@ from .core.object import DataProcessor
 
 def RAW_to_hdf(hdf_filename, RAW_path):
     processor = DataProcessor()
-    processor.load(hdf = hdf_filename, RAW = RAW_path)
-    processor.RAWtohdf()
+    processor.load(RAW = RAW_path)
+    processor.RAWtohdf(hdf_filename)
 
 def npy_to_hdf(hdf_filename, npy):
     pass
