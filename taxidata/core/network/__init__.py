d22c1d4339be28b10baa489b11ddf0a84fe2c35c
Kwon Yongsung <kwyosu27@gmail.com>
2019-12-06 15:19:31 +0900

    graph module

--- taxidata/core/network/__init__.py
+++ taxidata/core/network/__init__.py
@@ -4,6 +4,7 @@ import os
 import re
 import matplotlib.pyplot as plt
 
+<<<<<<< HEAD
 # edgelist data convert to npy
 def convert2npy_edgelist(path,filename):
     """Short summary.
@@ -71,6 +72,24 @@ def genStreetNet(Edgelist):
 
 
     """
+=======
+# edgelist, speed data convert to npy
+# def conver2npy_edgelist(path,filename):
+#     data = np.genfromtxt(path, delimiter=',',skip_header=1,dtype=[('Link', 'int'), ('Node_Start', 'int'), ('Longitude_Start', 'float'),('Latitude_Start', 'float'),('Node_End', 'int'), ('Longitude_End', 'float'),('Latitude_End', 'float'),('LENGTH', 'float')])
+#     np.save(filename,data)
+# def convert2npy_linkspeed(path):
+#     path_dr = path
+#     file_list = os.listdir(path_dr)
+#     file_list.sort()
+#     re_file_list = file_list[5:len((file_list))]
+#     for path_file in re_file_list:
+#         data = np.genfromtxt(path_file,delimiter=',',skip_header=1, dtype=[('Period','U12'),('Link','int'),('Speed','float')])
+#         filename=int(re.findall('\d+',path_file)[0])
+#         np.save('{}.npy'.format(filename),data)
+
+# generate Street network
+def genStreetNet(Edgelist):
+>>>>>>> graph module
     # node label & number
     node_list = np.unique(Edgelist['Node_Start'])
     # network generating
@@ -84,6 +103,7 @@ def genStreetNet(Edgelist):
 
 # generate newowrk nodes' position
 def network_pos(Edgelist):
+<<<<<<< HEAD
     """Short summary.
     Generate network node position.
     ex) pos=network_pos(Edgelist)
@@ -100,11 +120,14 @@ def network_pos(Edgelist):
 
 
     """
+=======
+>>>>>>> graph module
     # assign pos for nodes
     return {i:[Edgelist[Edgelist['Node_Start']==i]['Longitude_Start'][0],Edgelist[Edgelist['Node_Start']==i]['Latitude_Start'][0]]for i in range(len(np.unique(Edgelist['Node_Start'])))}
 
 # get max velocity each street link
 def Max_velocity(velocity0,velocity1):
+<<<<<<< HEAD
     """Short summary.
     Find each link's fatest speed in whole day.
     The reason why it takes two speed array is Chengdu's speed data splice day in two Period. 03:00~13:00, 13:00~23:00
@@ -118,6 +141,8 @@ def Max_velocity(velocity0,velocity1):
     type np.array(dtype=[('Period','U12'),('Link','int'),('Speed','float')])
 
     """
+=======
+>>>>>>> graph module
     max_velo = np.zeros(len(np.unique(velocity0['Link'])))
     for i in range(len(max_velo)):
         max_velo[i] = max([max(velocity0[velocity0['Link'] == i+1]['Speed']),max(velocity1[velocity1['Link'] == i+1]['Speed'])])
@@ -125,6 +150,7 @@ def Max_velocity(velocity0,velocity1):
 
 # get relative velocity
 def relativeVelocity(Period,velocity0,velocity1):
+<<<<<<< HEAD
     """Short summary.
     Divide road's each period speed by Fastest speed, get relative velocity each road
 
@@ -141,6 +167,8 @@ def relativeVelocity(Period,velocity0,velocity1):
 
 
     """
+=======
+>>>>>>> graph module
     return np.array(velocity0[velocity0['Period']==Period]['Speed']/Max_velocity(velocity0,velocity1))
 
 # generate network given weight by relative speed
@@ -170,7 +198,11 @@ def weaklycc(network):
     return [len(c) for c in sorted(nx.weakly_connected_components(network), key=len, reverse=True)]
 
 # measuring GCC, SCC, CPoint, and generating graph
+<<<<<<< HEAD
 def criticalGraph(day,Period,edgelist,speedlist0,speedlist1):
+=======
+def criticalGraph(Period,edgelist,speedlist0,speedlist1):
+>>>>>>> graph module
     # relative velocity
     rv = relativeVelocity(Period,speedlist0,speedlist1)
     # get GCC, SCC each q
@@ -192,5 +224,9 @@ def criticalGraph(day,Period,edgelist,speedlist0,speedlist1):
     curve2 = ax2.errorbar(q,cc[1]/1902,marker='^',markersize=20,label='SCC',c='orange')
     curves=[curve1,curve2]
     ax1.legend(curves,[curve.get_label()for curve in curves],fontsize='x-large')
+<<<<<<< HEAD
     plt.savefig('Chengdu_june{}_{}_ciritcalpoint_{}.png'.format(day,Period,criticalPoint),transparent=True,dpi=300)
+=======
+    plt.savefig('Chengdu_june1_{}_ciritcalpoint_{}.png'.format(Period,criticalPoint),transparent=True,dpi=300)
+>>>>>>> graph module
     plt.close()
