e729bc459c36c0cb8247b8a9535875b735b4efa0
junghoon.jung <jh.jung1380@gmail.com>
2020-02-03 21:40:30 +0900

    date int

--- taxidata/converters.py
+++ taxidata/converters.py
@@ -3,7 +3,7 @@ from .core.object import DataProcessor
 def RAW_to_hdf(hdf_filename, RAW_path, date):
     processor = DataProcessor()
     processor.load(RAW = RAW_path)
-    processor.set_date(date[0],date[1],date[2])
+    processor.set_date(int(date[0]),int(date[1]),int(date[2]))
     processor.RAWtohdf(hdf_filename)
 
 
