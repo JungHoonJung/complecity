ef43053c1756e932f0eb097242190139de8abefd
Kwon Yongsung <kwyosu27@gmail.com>
2020-02-11 13:53:56 +0900

    edit turning angle calculator

--- taxidata/core/network/generating_segment.py
+++ taxidata/core/network/generating_segment.py
@@ -214,6 +214,16 @@ node_Seoul = np.unique(Seoul['START_NODE']) # node 집합
 =======
 =======
 node_Seoul = np.unique(Seoul['START_NODE']) # node 집합
+"""
+define function
+"""
+def turning_angle(v1,v2):
+    magnitudeProduct = np.linalg.norm(v1)*np.linalg.norm(v2)
+    angle_innerProduct = math.acos(np.dot(v1,v2)/(magnitudeProduct+9e-12))
+    angle_vectorProduct = math.asin(np.cross(v1,v2)/magnitudeProduct)
+    if angle_vectorProduct == 0: angle = angle_innerProduct
+    else: angle = angle_vectorProduct/abs(angle_vectorProduct)*angle_innerProduct
+    return angle
 
 >>>>>>> edit)
 >>>>>>> edit)
@@ -323,8 +333,28 @@ def next_node(input):
                 X_3, Y_3 = Seoul[np.where(Seoul['START_NODE']==next_edge['END_NODE'][n])][0][0], Seoul[np.where(Seoul['START_NODE']==next_edge['END_NODE'][n])][0][1]
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 turning_angle = math.asin(((X_2-X_1)*(Y_3-Y_2)-(Y_2-Y_1)*(X_3-X_2))/(((X_2-X_1)**2+(Y_2-Y_1)**2)*((X_3-X_2)**2+(Y_3-Y_2)**2))**0.5)
 >>>>>>> segment generator
+=======
+                turning_angle = math.asin(((X_2-X_1)*(Y_3-Y_2)-(Y_2-Y_1)*(X_3-X_2))/(((X_2-X_1)**2+(Y_2-Y_1)**2)*((X_3-X_2)**2+(Y_3-Y_2)**2))**0.5)
+>>>>>>> segment generator
+=======
+                r1 = np.array([X_2-X_1, Y_2-Y_1])
+                r2 = np.array([X_3-X_2, Y_3-Y_2])
+                turningAngle = turning_angle(r1,r2)
+            if turningAngle==np.pi and a[n][2]<0: turningAngle*=-1
+>>>>>>> edit turning angle calculator
+        b.append([a[n][0] + "+" + str(next_edge['END_NODE'][n]), a[n][1] + next_edge['LENGTH'][n],a[n][2]+turning_angle])
+    return b
+
+def node_cycle(input):
+    output=[]
+    for i in range(len(input)):
+        output = output + next_node(input[i])
+    return output
+<<<<<<< HEAD
+>>>>>>> edit turning angle calculator
 <<<<<<< HEAD
 >>>>>>> since problem7
 =======
@@ -652,8 +682,9 @@ SN = node_Seoul[1002]
 k = 800
 =======
 """
-main function
+main
 """
+<<<<<<< HEAD
 k = 800 # k=500m
 start = time.time()
 >>>>>>> edit)
@@ -663,6 +694,14 @@ SN = node_Seoul[1002]
 #[+node+node+..., total_length, turning_angle]
 <<<<<<< HEAD
 >>>>>>> segment generator
+=======
+k = 300 # k=300m
+segment_int = [] # segment set
+for SN in node_Seoul[1000:1001]:
+    segmnet=[]
+    start = time.time()
+    #[+node+node+..., total_length, turning_angle]
+>>>>>>> edit turning angle calculator
     input=[["+" + str(SN), 0, 0]]
     count=0
     while count < 100 :
@@ -671,10 +710,14 @@ SN = node_Seoul[1002]
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> edit turning angle calculator
 =======
 <<<<<<< HEAD
 >>>>>>> segment generator
+=======
+<<<<<<< HEAD
+>>>>>>> edit turning angle calculator
         for i in range(len(input)):
             i-=pop_parameter
             check_turnBack = input[i][0].split('+')
@@ -730,7 +773,15 @@ SN = node_Seoul[1002]
         for i in range(len(input)):
             i-=pop_parameter
 >>>>>>> segment generator
+<<<<<<< HEAD
 >>>>>>> segment generator
+=======
+=======
+        for i in range(len(input)):
+            i-=pop_parameter
+            check_turnBack = input[i][0].split('+')
+>>>>>>> edit turning angle calculator
+>>>>>>> edit turning angle calculator
             # if path's length exceed k
             if input[i][-2]>k:
                 segment.append(input[i])
@@ -754,6 +805,7 @@ SN = node_Seoul[1002]
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> edit turning angle calculator
 =======
@@ -776,6 +828,10 @@ SN = node_Seoul[1002]
 >>>>>>> last..
 =======
 >>>>>>> segment generator
+=======
+=======
+>>>>>>> edit turning angle calculator
+>>>>>>> edit turning angle calculator
 =======
 =======
 >>>>>>> segment generator
@@ -803,6 +859,7 @@ SN = node_Seoul[1002]
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> since 9
 =======
@@ -818,6 +875,8 @@ SN = node_Seoul[1002]
 =======
 =======
 >>>>>>> since problem7
+=======
+>>>>>>> edit turning angle calculator
 =======
 =======
 >>>>>>> segment generator
@@ -1014,3 +1073,5 @@ for i in range(len(segment)):
 >>>>>>> segment generator
 =======
 >>>>>>> edit)
+=======
+>>>>>>> edit turning angle calculator
