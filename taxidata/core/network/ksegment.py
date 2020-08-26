d84f63dc707fc112bd21ae471601eff7a39fe6e7
kwyosu7 <kwyosu27@gmail.com>
2020-08-26 15:22:21 +0900

    change wrong code

--- taxidata/core/network/ksegment.py
+++ taxidata/core/network/ksegment.py
@@ -90,7 +90,8 @@ class Segment:
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return np.array([self.start_node == other.start_node, self.path==other.path]).all()
+            return all([self.start_node == other.start_node, (self.path==other.path).min()])
+            # ? start_node도 봐야하나?? path끼리만 비교해도 되지 않으려나?
         else:
             return False
 
